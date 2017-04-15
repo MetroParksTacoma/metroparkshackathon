98f69f4e6c8193b3714d3673af476bd70a923db5
John <John.Dennis.Jackson@gmail.com>
2017-04-14 22:04:09 -0700

    wtf take 2

--- flask-app/api.py
+++ flask-app/api.py
@@ -5,7 +5,7 @@ import flask.ext.restless
 # Create the Flask application and the Flask-SQLAlchemy object.
 app = flask.Flask(__name__, static_url_path="")
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = flask.ext.sqlalchemy.SQLAlchemy(app)
 
 @app.route('/')
